c693048017645008a6b414ccb625cf324956b33a
Iago Alonso <iago.alonso@marfeel.com>
2024-07-08 12:53:09 +0200

    feat: Delay requests by 250 ms

--- v2/main.go
+++ v2/main.go
@@ -258,6 +258,8 @@ func (c *Client) DeletePersonalAccessToken(ctx context.Context, uuid string) err
 // --------
 func (c *Client) sendRequest(ctx context.Context, method string, url string, body []byte, result interface{}) error {
 
+	time.Sleep(250 * time.Millisecond)
+
 	authJson, err := json.Marshal(c.auth)
 	if err != nil {
 		return err
